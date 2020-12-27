d6ceab1b2f5cc501c0dccfb599a18cd1177177ed
Logan Caraco <sybel@blue-pop-os.lan>
2020-12-26 18:03:27 -0800

    cleanup
    * BayesNetParser.hs:

--- BayesNetParser.hs
+++ BayesNetParser.hs
@@ -7,27 +7,16 @@
 module BayesNetParser where
 
 import qualified Text.Parsec as P
-import qualified Text.Parsec.Char as C
 
 import Control.Monad (void)
 import Control.Applicative ((<|>), many)
 
 import Text.Parsec (ParseError, try, manyTill, lookAhead)
-import Text.Parsec.Char (digit, string)
-import Text.Parsec.Combinator (eof, many1, sepBy)
+import Text.Parsec.Char (noneOf, oneOf, digit, string)
+import Text.Parsec.Combinator (eof, many1)
 import Text.Parsec.String (Parser)
 
 import System.Environment (getArgs)
-
-oneOf :: [Char] -> Parser Char
-oneOf = C.oneOf
-
-noneOf :: [Char] -> Parser Char
-noneOf = C.noneOf
-
--- from example code
-parse :: Parser a -> String -> Either ParseError a
-parse p = P.parse p ""
 
 -- from example code
 parseWithEof :: Parser a -> String -> Either ParseError a
@@ -52,13 +41,16 @@ main =
           fText <- readFile str
           let pRes = parseWithEof script fText
             in either print print pRes
-      _ -> error "please provide file path"
+      _ -> error "please provide parse file path"
 
 whitespace :: Parser ()
 whitespace = void . many $ oneOf whitespaceChars
 
-wsDelimList :: Parser a -> Parser [a]
-wsDelimList p = try p `sepBy` (many1 $ oneOf " \t") 
+eol :: Parser String
+eol =   try (string "\n\r")
+    <|> try (string "\r\n")
+    <|> string "\n"
+    <|> string "\r"
 
 lexeme :: Parser a -> Parser a
 lexeme p = p <* whitespace
@@ -66,14 +58,11 @@ lexeme p = p <* whitespace
 num :: Parser String
 num = many digit
 
-symbol :: Parser String
-symbol = many1 $ noneOf (whitespaceChars ++ "[]")
-
 symLexeme :: Parser String
-symLexeme = lexeme $ symbol 
+symLexeme = lexeme $ many1 $ noneOf (whitespaceChars ++ "[]")
 
-strLexeme :: String -> Parser String
-strLexeme = lexeme . string
+strLexeme :: String -> Parser ()
+strLexeme = void . lexeme . string
   
 data Node =
   Node { name :: String, values :: [String] } deriving Show
@@ -84,19 +73,18 @@ data TableEntry =
   TableEntry { conditions :: [Condition],  probabilities :: [Probability] } deriving Show
 type Table = (String, [TableEntry])
 
+untilDashes :: Parser a -> Parser [a]
+untilDashes p = manyTill p $ strLexeme "---" 
+
+block :: String -> Parser a -> Parser [a]
+block s p = strLexeme s *> untilDashes p
+
 node :: Parser Node
 node =
   do
     name <- symLexeme
-    values <- wsDelimList symbol
-    whitespace
+    values <- manyTill symLexeme eol 
     return $ Node { name, values }
-
-untilDashes :: Parser a -> Parser [a]
-untilDashes p = manyTill p (strLexeme "---") 
-
-block :: String -> Parser a -> Parser [a]
-block s p = strLexeme s *> untilDashes p
 
 nodeBlock :: Parser [Node]
 nodeBlock = block "Nodes" node
@@ -108,53 +96,34 @@ edgeBlock :: Parser [Edge]
 edgeBlock = block "Edges" edge
 
 condition :: Parser Condition
-condition = 
-  do
-    name <- symLexeme
-    strLexeme "="
-    val <- symLexeme
-    return (name, val)
+condition = (,) <$> symLexeme <*> (strLexeme "=" *> symLexeme)
 
 probFloat :: Parser Float
 probFloat = read <$> lexeme prob
   where
-    prob = string "1" <|> decimal
+    prob = string "1" <|> try decimal <|> string "0"
     decimal = (++) <$> string "0." <*> many1 digit
 
 probability :: Parser Probability
-probability =
-  do
-    name <- symLexeme
-    strLexeme "="
-    val <- probFloat
-    return (name, val)
+probability = (,) <$> (symLexeme <* strLexeme "=") <*> probFloat
 
-squareWsList :: Parser a -> Parser [a]
-squareWsList p = strLexeme "[" *> manyTill p (strLexeme "]")
+squareList :: Parser a -> Parser [a]
+squareList p = strLexeme "[" *> manyTill p (strLexeme "]")
 
 tableEntry :: Parser TableEntry
 tableEntry =
   do
-    conditions <- squareWsList condition
-    probabilities <- manyTill probability (lookAhead tableEntryTerminator)
+    conditions <- squareList condition
+    probabilities <- manyTill probability $ lookAhead tableEntryTerminator
     return $ TableEntry { conditions, probabilities }
     where tableEntryTerminator = string "[" <|> string "---"
 
 cptBlock :: Parser Table
-cptBlock =
-  do
-    strLexeme "CPT"
-    name <- symLexeme
-    entries <- untilDashes $ tableEntry <* whitespace
-    return (name, entries)
+cptBlock = (,) <$> (strLexeme "CPT" *> symLexeme) <*> untilDashes tableEntry
+
+cpts :: Parser [Table]
+cpts = strLexeme "TABLES" *> many cptBlock
 
 script :: Parser ([Node], [Edge], [Table])
-script =
-  do
-    whitespace
-    nodes <- nodeBlock
-    edges <- edgeBlock
-    strLexeme "TABLES"
-    tables <- many1 cptBlock
-    return (nodes, edges, tables)
+script = whitespace *> ((,,) <$> nodeBlock <*> edgeBlock <*> cpts)
 
