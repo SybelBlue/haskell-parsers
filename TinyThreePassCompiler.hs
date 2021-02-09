3bd1634c7ebc895721440329f374a11310cf3dfd
L Caraco <lbc.dna@gmail.com>
2021-02-08 19:49:02 -0600

    pass 1 with parser

--- TinyThreePassCompiler.hs
+++ TinyThreePassCompiler.hs
@@ -3,7 +3,9 @@
 {-# LANGUAGE TupleSections #-}
 module TinyThreePassCompiler where
 
+import Debug.Trace
 
+import Data.List
 import Data.Bifunctor
 import Control.Monad
 import Control.Applicative
@@ -44,12 +46,8 @@ splitAtFirst :: (a -> Bool) -> [a] -> ([a], [a])
 splitAtFirst p = either (,[]) id . foldl folder (Left []) 
   where folder (Left xs) x = if p x then Right (xs, []) else Left (x:xs)
         folder (Right (xs, ys)) y = Right (xs, y:ys)
-
 pass1 :: String -> AST
-pass1 s = do
-  let tokens = tokenize s
-  let (argList, tokens) = splitAtFirst ((==) $ TChar ']') tokens
-  undefined
+pass1 = coerceParse function . tokenize
 
 pass2 :: AST -> AST
 pass2 = undefined
@@ -57,27 +55,10 @@ pass2 = undefined
 pass3 :: AST -> [String]
 pass3 = undefined
 
-
--- function   ::= '[' arg-list ']' expression
-
--- arg-list   ::= /* nothing */
---               | variable arg-list
-
--- expression ::= term
---               | expression '+' term
---               | expression '-' term
-
--- term       ::= factor
---               | term '*' factor
---               | term '/' factor
-
--- factor     ::= number
---               | variable
---               | '(' expression ')'
-
-
-
 newtype Parser a b = Parser { parse :: [a] -> Either String (b, [a]) }
+
+coerceParse :: Parser a b -> [a] -> b
+coerceParse p = (\(Right (b, _)) -> b) . parse p
 
 instance Functor (Parser a) where
     fmap fb p = Parser $ second (first fb) . parse p
@@ -107,13 +88,48 @@ instance Alternative (Parser a) where
           x -> x
       x -> x
 
-oneOf :: (Eq a, Show a) => [a] -> Parser a a
-oneOf as = Parser $ \case
-  [] -> Left "empty stream"
-  (x:xs) | x `elem` as -> Right (x, xs)
-  (x:xs) -> Left (show x ++ " does not match any of " ++ show xs)
+parser :: (a -> Either String b) -> Parser a b
+parser f = Parser inner
+  where inner (x:xs) = second (, xs) (f x)
+        inner [] = Left "empty stream"
 
-numberLiteral :: Parser Char Int
-numberLiteral = read <$> many (oneOf digit)
+number :: Parser Token AST
+number = parser num
+  where num (TInt x) = Right (Imm x)
+        num _        = Left "not a number"
 
+identifier :: Parser Token String
+identifier = parser ident
+  where ident (TStr s) = Right s
+        ident _        = Left "not an identifier"
 
+variable :: [String] -> Parser Token AST
+variable vars = do
+  name <- identifier
+  maybe (fail $ "unkown symbol " ++ name) (return . Arg) (name `elemIndex` vars)
+
+char :: Char -> Parser Token Char
+char c = parser chr
+  where chr (TChar x) | x == c = Right c
+        chr _                  = Left $ "token is not '" ++ c:"'"
+
+betweenChars :: (Char, Char) -> Parser Token b -> Parser Token b
+betweenChars (a, b) p = char a *> p <* char b
+
+function :: Parser Token AST
+function = expression =<< betweenChars ('[', ']') (many identifier)
+
+expression :: [String] -> Parser Token AST
+expression args = add <|> sub <|> term args
+  where add = Add <$> term args <* char '+' <*> term args
+        sub = Sub <$> term args <* char '-' <*> term args
+
+term :: [String] -> Parser Token AST
+term args = mul <|> div <|> factor args
+  where mul = Mul <$> factor args <* char '*' <*> term args
+        div = Div <$> factor args <* char '/' <*> term args
+
+factor :: [String] -> Parser Token AST
+factor args = number <|> variable args <|> betweenChars ('(', ')') (expression args)
+
+main = print $ pass1 "[ a b ] a*a + b*b"
