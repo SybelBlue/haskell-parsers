7c8e21bcf830d93cef48afbcea20f7a38214d2d0
L Caraco <lbc.dna@gmail.com>
2021-04-09 14:31:24 -0500

    basics

--- SqlParser.hs
+++ SqlParser.hs
@@ -1,5 +1,10 @@
 module SqlParser where
 
+import Control.Monad
+
+import Text.Parsec
+import Text.Parsec.String
+import Data.Functor
 
 {-
 query         =  select, ws, from, [ ws, join ], [ ws, where ] ;
@@ -16,3 +21,35 @@ comparison    =  " = " | " > " | " < " | " <= " | " >= " | " <> " ;
 const         =  ? a number ? | ? a SQL single-quoted string ? ;
 ws            = " " | "\n" | ws, ws ;
 -}
+
+data Comparison 
+    = Eq
+    | Lt
+    | Gt
+    | Le
+    | Ge
+    | Dm
+    | Ne
+    deriving (Show, Eq)
+
+comparison :: Parser Comparison
+comparison = char ' ' *> (
+    (char '=' $> Eq) <|>
+    (char '>' $> Lt) <|>
+    (char '<' $> Gt) <|>
+    (string "<=" $> Le) <|>
+    (string ">=" $> Ge) <|>
+    (string "<>" $> Ne)
+    ) <* char ' '
+
+
+whitespace = void . many $ oneOf " \n"
+
+identifier :: Parser String
+identifier = many1 (alphaNum <|> char '_') <* whitespace
+
+tableName = identifier
+
+columnName = identifier
+
+columnId = (,) <$> identifier <*> (char '.' >> identifier)
