a24444082bf8c009cf4ca3a326b26eb2cbc035ea
L Caraco <lbc.dna@gmail.com>
2021-04-17 21:04:57 -0500

    working on sql

--- SqlParser.hs
+++ SqlParser.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE FlexibleContexts #-}
 module SqlParser where
 
@@ -8,8 +9,12 @@ import Text.Parsec.String
 import Data.Functor
 import Data.Char
 
+import qualified Data.Map.Strict as M
+import Data.Bifunctor
+import Data.Either (fromRight)
+
 {-
-query         =  select, ws, from, [ ws, join ], [ ws, where ] ;
+* query         =  select, ws, from, [ ws, join ], [ ws, where ] ;
 * select        =  "SELECT ", column-id, [ { ", ", column-id } ] ;
 * from          =  "FROM ", table-name, [ { ws, join } ] ;
 * join          =  "JOIN ", table-name, " on ", value-test ;
@@ -26,7 +31,7 @@ query         =  select, ws, from, [ ws, join ], [ ws, where ] ;
 type ValueTest = (SQLValue, BinOp, SQLValue)
 
 data SQLValue
-    = ColId (String, String)
+    = ColId ColumnID
     | Quote String
     | Number Float
     deriving (Show, Eq)
@@ -41,13 +46,33 @@ data BinOp
     | Ne
     deriving (Show, Eq)
 
-select = symbol "SELECT" *> columnName `sepBy1` (char ',' <* many1 whitespace)
+type ColumnID = (String, String)
+type TableName = String
+type Join = (TableName, ValueTest)
+type From = (TableName, [Join])
+
+data Query = Query { select :: [ColumnID], from :: From, joins :: [Join], wheres :: [ValueTest] } deriving Show
+
+type Table = [M.Map String String]
+type Database = M.Map String Table
+
+database :: [(String, [[(String, String)]])] -> Database
+database = M.fromList . map (second $ map M.fromList) 
+
+queryP = do
+    select <- selectP
+    from <- fromP
+    joins <- many joinP
+    wheres <- many whereP
+    return $ Query { select, from, joins, wheres }
+
+selectP = symbol "SELECT" *> columnId `sepBy1` (char ',' <* many1 whitespace)
 
 whereP = symbol "WHERE" *> valueTest
 
-joinP = (,) <$> (symbol "JOIN" *> tableName) <*> (symbol "on" *> valueTest)
-
 fromP = (,) <$> (symbol "FROM" *> tableName) <*> (joinP `sepBy` many1 whitespace)
+
+joinP = (,) <$> (symbol "JOIN" *> tableName) <*> (symbol "on" *> valueTest)
 
 comparison :: Parser BinOp
 comparison = char ' ' *> (
@@ -93,7 +118,47 @@ numeric = do
     tail <- ((:) <$> char '.' <*> many1 digit) <|> return []
     return $ read (head ++ tail)
 
+getQuery :: String -> Query
+getQuery s = case parse queryP "" s of
+    Left e -> error (show e)
+    Right v -> v
+
 sqlEngine :: [(String,[[(String,String)]])] -> String -> [[(String,String)]]
-sqlEngine database = execute where
-  execute :: String -> [[(String,String)]]
-  execute query = undefined
+sqlEngine = execute . database 
+
+execute :: Database -> String -> [[(String,String)]]
+execute db q = do
+    let query = getQuery q
+    undefined 
+
+
+rawMovieDatabse = [ ( "movie"
+                  , [ [ ( "id", "1" ), ( "name", "Avatar"   ), ( "directorID", "1" ) ]
+                    , [ ( "id", "2" ), ( "name", "Titanic"  ), ( "directorID", "1" ) ]
+                    , [ ( "id", "3" ), ( "name", "Infamous" ), ( "directorID", "2" ) ]
+                    , [ ( "id", "4" ), ( "name", "Skyfall"  ), ( "directorID", "3" ) ]
+                    , [ ( "id", "5" ), ( "name", "Aliens"   ), ( "directorID", "1" ) ]
+                    ]
+                  )
+                , ( "actor"
+                  , [ [ ( "id", "1" ), ( "name", "Leonardo DiCaprio" ) ]
+                    , [ ( "id", "2" ), ( "name", "Sigourney Weaver"  ) ]
+                    , [ ( "id", "3" ), ( "name", "Daniel Craig"      ) ]
+                    ]
+                  )
+                , ( "director"
+                  , [ [ ( "id", "1" ), ( "name", "James Cameron"   ) ]
+                    , [ ( "id", "2" ), ( "name", "Douglas McGrath" ) ]
+                    , [ ( "id", "3" ), ( "name", "Sam Mendes"      ) ]
+                    ]
+                  )
+                , ( "actor_to_movie"
+                  , [ [ ( "movieID", "1" ), ( "actorID", "2" ) ]
+                    , [ ( "movieID", "2" ), ( "actorID", "1" ) ]
+                    , [ ( "movieID", "3" ), ( "actorID", "2" ) ]
+                    , [ ( "movieID", "3" ), ( "actorID", "3" ) ]
+                    , [ ( "movieID", "4" ), ( "actorID", "3" ) ]
+                    , [ ( "movieID", "5" ), ( "actorID", "2" ) ]
+                    ]
+                  )
+                ]
